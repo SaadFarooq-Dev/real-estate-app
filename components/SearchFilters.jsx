5ceea60c0f9f792861c314ef9ea047dcb19bc575
Saad Farooq <saadfarooq.info@gmail.com>
2023-03-21 02:11:47 +0500

    feat: added search query params;

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+import { useRouter } from "next/router";
+import { Box, Button, Flex, Icon, Input, Select, Spinner, Text } from "@chakra-ui/react";
+import {MdCancel} from 'react-icons/md'
+import Image from "next/image";
+
+
+const SearchFilters = () => {
+  return (
+    <div>
+      searchFilters
+    </div>
+   );
+}
+
+export default SearchFilters;
